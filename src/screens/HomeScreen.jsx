204c4aede2277c165cdea321bcd936578fbe191c
Tyler Hammer <tyler.hammer@sendbird.com>
2023-10-24 13:40:10 +0900

    chore: Remove unused imports

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -2,10 +2,8 @@ import {SafeAreaView, StyleSheet, View, useColorScheme} from 'react-native';
 import LogoFooter from '../components/common/LogoFooter';
 import HomeContent from '../components/home/HomeContent';
 import HomeHeader from '../components/home/HomeHeader';
-import {COLORS} from '../constants';
 
 export default function HomeScreen() {
-  const selectedTheme = useColorScheme();
   return (
     <SafeAreaView style={styles.screen}>
       <View style={styles.container}>
--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -2,10 +2,8 @@ import {SafeAreaView, StyleSheet, View, useColorScheme} from 'react-native';
 import LogoFooter from '../components/common/LogoFooter';
 import LoginForm from '../components/login/LoginForm';
 import LoginHeader from '../components/login/LoginHeader';
-import {COLORS} from '../constants';
 
 export default function LoginScreen() {
-  const selectedTheme = useColorScheme();
   return (
     <SafeAreaView style={styles.screen}>
       <View style={styles.container}>
